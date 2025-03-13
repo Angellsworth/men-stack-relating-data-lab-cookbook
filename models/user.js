6767cfed0d67c176500300cbee1436d263f50b47
Angellsworth <angellsworth@msn.com>
2025-03-12 20:30:12 -0600

    wip: Updated user model to include embedded pantry schema

--- models/user.js
+++ models/user.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 
+//define foodSchema first
+const foodSchema = new mongoose.Schema({
+  name: {
+    type: String, required: true
+  }
+})
+//embed foodSchema INSIDE userSchema
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -9,6 +16,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  pantry: [foodSchema]//embed foodSchema
 });
 
 const User = mongoose.model('User', userSchema);
