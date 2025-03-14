c7c302c64fab9b60027871a10336f264e4e94bbc
Angellsworth <angellsworth@msn.com>
2025-03-13 23:36:08 -0600

    wip: added a community page

--- controllers/community.js
+++ controllers/community.js
@@ -1,3 +1,4 @@
+// controllers/community.js
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user.js');
@@ -6,10 +7,26 @@ const User = require('../models/user.js');
 router.get('/', async (req, res) => {
     try {
         const allUsers = await User.find({}, 'username')//get all users, only user names
-        res.render('community/index.ejs', { users })
+        res.render('users/index.ejs', { users: allUsers })
     } catch (error) {
         console.error('Error fetching users:', error)
         res.redirect('/')
+    }
+})
+//GET /community/:userId - Show SPECIFIC user's pantry
+router.get('/:userId', async (req, res) => {
+    try {
+        const selectedUser = await User.findById(req.params.userId).select('username pantry')
+
+        if(!selectedUser) {
+            console.log('User not found, redirecting...');
+            return res.redirect('/community');
+        }
+        res.render('users/show.ejs', { selectedUser })
+
+    } catch (error) {
+        console.error('Error finding user pantry:', error)
+        res.redirect('/community')
     }
 })
 
